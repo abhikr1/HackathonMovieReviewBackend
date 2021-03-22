42b42e722a225b098f47d91d9f826b0a162e2a46
Abhinav <abhikrofficial@gmail.com>
2021-03-22 20:27:18 +0530

    Review.js updated

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -29,19 +29,20 @@ router.get('/:moviename', (req,res) => {
 
 
 
-router.post('/myratings', (req, res) => {
+router.post('/myratings', auth.authenticate, (req, res) => {
+    console.log("Hi");
     console.log(req.body.movie_name);
-    req.session.success = true;
-    // if (!req.session.userId) {
-    //     res.send(401).send({message : "Not logged in"});
-    //     return;
-    // }
-    // if (!req.body.rating) {
-    //     res.status(400).send({message: "Please enter the rating"});
-    //     return;
-    //
+    let email;
+    if (!req.session.userId) {
+        res.send(401).send({message : "Not logged in"});
+        return;
+    }
+    if (!req.body.rating) {
+        res.status(400).send({message: "Please enter the rating"});
+        return;
+    }
 
-    const {movie_name, rating, description, email } = req.body;
+    const {movie_name, rating, description} = req.body;
     console.log(req.body);
     if (!rating) {
         res.status(200).send({message: "Please provide Rating"});
@@ -52,29 +53,25 @@ router.post('/myratings', (req, res) => {
         res.status(200).send({message: "Enter a description"});
         return;
     }
-     
-    // email_id = UserCredential.findOne({ _id : req.session.userId}).then(User => {
-        // if (User) {
-            
+    email_id = UserCredential.findOne({ _id : req.session.userId}).then(User => {
+        if (User) {
+            email = User.email
 
         ReviewDetails.findOne({ email, moviename : movie_name }).then(user => {
         if (user) {
-            res.status(200).send({message: "Rating for this movie is already added by the user"});
+            res.status(400).send({error: "Rating for this movie is already added by the user"});
             return;
         }
 
 
         const review = new ReviewDetails({ email, moviename : movie_name, feedbackdesc : description, rating });
-        console.log(email);
-        console.log(description);
-        console.log(rating);
         review.save().then(() => {
                 res.status(201).send({message : 'Feedback Submitted'});
             });
     }).catch(() => {
-        res.status(500).send({ message: "Internal Server Error" });
+        res.status(500).send({ error: "Internal Server Error" });
     });
-   
-
+}    
+});
 });
 module.exports = router;
--- server/routes/sessions.js
+++ server/routes/sessions.js
@@ -33,7 +33,6 @@ router.post('/', (req, res) => {
             res.status(400).send({message: "Incorrect email or password"});
             return;
         }
-        req.session.success = true;
         req.session.userId = user.id;
         console.log(req.session.userId);
 
